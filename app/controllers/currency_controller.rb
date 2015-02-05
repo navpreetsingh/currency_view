06394b06fce52ed215e3ff771be5a1262506ee95
trantor-navpreet-singh <navpreetsingh21@gmail.com>
2015-02-05 12:29:25 +0530

    basic view done

--- app/controllers/currency_controller.rb
+++ app/controllers/currency_controller.rb
@@ -1,13 +1,14 @@
 class CurrencyController < ApplicationController
   def view
-    default = (0..10).step(0.1).to_a
-    sql1 = "SELECT avg(max) as max_avg avg(min) as min_avg"
-    default.each do |d|
-      sql1 += "sum(if(max > #{d},1,0)) as max_#{d}"
-      sql1 += "sum(if(min > #{d},1,0)) as min_#{d}"
-    end
-    sql = sql1 + "FROM  `xau_usd1_mins`"
-    XauUsd1Min
-
+    @default = (0.1..10).step(0.1).to_a
+    sql1 = "SELECT avg(max) as max_avg, avg(min) as min_avg, count(*) as total"
+    @default.each do |d|
+      sql1 += ", sum(if(max >= #{d.round(1)},1,0)) as max_#{d.round(1).to_s.gsub(".", "")}"
+      sql1 += ", sum(if(min >= #{d.round(1)},1,0)) as min_#{d.round(1).to_s.gsub(".", "")}"
+    end      
+    sql = sql1 + " FROM  `xau_usd1_mins`"
+    # debugger
+    @data = XauUsd1Min.connection.execute(sql).first
+    # puts "great"
   end
 end
--- lib/data_storing.rb
+++ lib/data_storing.rb
@@ -1,16 +1,16 @@
 class DataStoring
   class << self
     def store
-      # file = File.read("/home/trantor/personal_projects/currency_market/xauusd_1_min_8th_dec").split("\n")
-      # file = file.map{|f| f.split("\t")}      
-      # file.each do |f|
-      #   begin
-      #     XauUsd1Min.create(:date => f[0], :open => f[1], :high => f[2], :low => f[3], :close => f[4], 
-      #       :change_pips => f[5], :change_percentage => f[6])
-      #   rescue Exception => e
-      #     puts "Exception"
-      #   end
-      # end
+      file = File.read("/home/trantor/personal_projects/currency_market/xauusd_1_min_8th_dec").split("\n")
+      file = file.map{|f| f.split("\t")}      
+      file.each do |f|
+        begin
+          XauUsd1Min.create(:date => f[0], :open => f[1], :high => f[2], :low => f[3], :close => f[4], 
+            :change_pips => f[5], :change_percentage => f[6])
+        rescue Exception => e
+          puts "Exception"
+        end
+      end
       sql = File.read(Rails.root + "lib/sql_files/more_data.sql").split("\n")
       sql.each do |s|
         XauUsd1Min.connection.execute(s)
